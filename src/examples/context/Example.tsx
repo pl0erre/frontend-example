fcdf0eebf5c95550428ee3b597510f9f285810db
Stefan Freudenreich <stefan.freudenreich@agido.com>
2019-05-15 14:28:44 +0200

    Context with hooks

--- src/examples/context/Example.tsx
+++ src/examples/context/Example.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import MatchList from './components/MatchList';
+import {MatchListWithConsumer, MatchListWithHooks} from './components/MatchList';
 import {ProgramContext, ProgramState, ProgramDefaultState} from './ProgramContext';
 import * as ProgramAPI from '../../api/program/API';
 
 interface Props {}
-type State = ProgramState;
+type State = ProgramState & {
+    useHooks: boolean;
+};
 
 class App extends React.Component<Props, State> {
-    state = ProgramDefaultState;
+    state = {
+        ...ProgramDefaultState,
+        useHooks: false
+    };
 
     private getMatches = async () => {
         const matches = await ProgramAPI.getMatches();
@@ -17,10 +22,17 @@ class App extends React.Component<Props, State> {
         });
     };
 
+    private toggleUseHooks = () => {
+        this.setState(prevState => ({
+            useHooks: !prevState.useHooks
+        }));
+    };
+
     public render() {
         return (
             <ProgramContext.Provider value={{matches: this.state.matches, getMatches: this.getMatches}}>
-                <MatchList />
+                <button onClick={this.toggleUseHooks}>Toggle Consumer/Hooks</button>
+                {this.state.useHooks ? <MatchListWithHooks /> : <MatchListWithConsumer />}
             </ProgramContext.Provider>
         );
     }
--- src/examples/context/components/MatchList.tsx
+++ src/examples/context/components/MatchList.tsx
@@ -2,4 +2,11 @@ import React from 'react';
 import MatchList from '../../common/components/MatchList';
 import {ProgramContext} from '../ProgramContext';
 
-export default () => <ProgramContext.Consumer>{state => <MatchList {...state} />}</ProgramContext.Consumer>;
+export const MatchListWithConsumer: React.FC = () => (
+    <ProgramContext.Consumer>{state => <MatchList {...state} />}</ProgramContext.Consumer>
+);
+
+export const MatchListWithHooks: React.FC = () => {
+    const state = React.useContext(ProgramContext);
+    return <MatchList {...state} />;
+};
